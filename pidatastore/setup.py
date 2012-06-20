18e4ddbcbe93e783339349c275f0fccdcde43bf5
Russell Hay <russell.hay@gmail.com>
2012-06-20 19:54:00 -0400

    Setting up pidatastore setup

---
+++ pidatastore/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+   name = 'pidatastore',
+   version = '0.1dev',
+   packages = find_packages(),
+   
+   author = 'Russell Hay',
+   author_email = 'me@russellhay.com',
+   description = 'Datastore required by PhotoIndex',
+   url = 'http://russellhay.com/photoindex'
+)
