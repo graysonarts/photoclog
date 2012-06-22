c04015b6b1039c5c0b07ce99162641f1338f6ac0
Russell Hay <russell.hay@gmail.com>
2012-06-21 21:31:47 -0400

    Fixing the version number so we can just use egg_info

--- pidatastore/setup.py
+++ pidatastore/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
    name = 'pidatastore',
-   version = '0.1dev',
+   version = '0.1',
    packages = find_packages(),
    
    author = 'Russell Hay',
